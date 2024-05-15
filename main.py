0c7c1297bb9da041282e8370d951d50920d5ca6b
annasibii <160798608+annasibii@users.noreply.github.com>
2024-05-15 02:46:50 +0000

    Added my logo image

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from tkinter import*
 
+#Created a window and set a background colour
 window = Tk()
 window.geometry("300x300")
 window.title("My App")
 window['bg']='#2C2A64'
+
+#Added my logo image and edited the appearance
+img = PhotoImage(file="applogo.png")
+label = Label(window,image=img, borderwidth=0, highlightthickness=0 )
+label.pack()
+
+
 window.mainloop()
 
