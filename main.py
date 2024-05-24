770914c939d6b01dbd51ab21253b8796f803e2c5
annasibii <160798608+annasibii@users.noreply.github.com>
2024-05-24 00:32:19 +0000

    Resized image/logo and placed it to the top left

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ window['bg']='#2C2A64'
 
 #Added my logo image and edited the appearance
 img = PhotoImage(file="applogo.png")
-label = Label(window,image=img, borderwidth=0, highlightthickness=0 )
-label.pack()
+#This resizes the orginial picture by a factor of 6
+img = img.subsample(6) 
+#This allows the gray border of the picture is disappear
+label = Label(window,image=img,borderwidth=0, highlightthickness=0)
+#This allows the picture to be found on the left and north west of the page
+label.pack(side=LEFT, anchor=NW)
+
+
+
 
 
 window.mainloop()
