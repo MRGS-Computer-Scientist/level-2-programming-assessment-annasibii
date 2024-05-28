e9367ea2d54a6700f683d651a7451a23d1e5fe97
annasibii <160798608+annasibii@users.noreply.github.com>
2024-05-28 22:47:14 +0000

    Repositioned "My Goalie" and created new label2

--- main.py
+++ main.py
@@ -16,15 +16,21 @@ label = Label(window,image=img,borderwidth=0, highlightthickness=0)
 #This allows the picture to be found on the left and north west of the page
 label.pack(side=LEFT, anchor=NW)
 
-text_label = Label(window, text="My Goalie", font=("Arial", 80), fg="#98D8DD", bg="#2C2A64")
-text_label.place(relx=0.5, rely=0.5, anchor=CENTER)
+#Text displaying app name
+text_label = Label(window, text="My Goalie", font=("Georgia", 48), fg="#98D8DD", bg="#2C2A64")
+text_label.place(relx=0.09, rely=0.01, anchor=NW)
+
+text_label2 = Label(window, text="Are you curious to discover and work towards a personalised goal?", font =("Verdana", 25),
+fg="#FBFF37", bg="#2C2A64")
+text_label2.place(relx=0.5, rely=0.4, anchor=CENTER)
+
 
 #This allows for a new window to open when the button is clicked
 def open_quiz_window():
     quiz_window.create_quiz_window(window)
 
 #Created a button to take quiz + designed the appearance
-quiz_button = Button(window,text= "Take the Quiz",command=open_quiz_window, font=("Arial", 40), bg='#A20202', fg='white', borderwidth=7,)
+quiz_button = Button(window,text= "Take the Quiz",command=open_quiz_window, font=("Arial", 37), bg='#A20202', fg='white', borderwidth=7,)
 quiz_button.place(relx=0.5, rely=0.8, anchor='s')
 
 
