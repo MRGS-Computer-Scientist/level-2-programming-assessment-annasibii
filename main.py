703cc867bd712d374457f85bf52268032f25b8ad
annasibii <160798608+annasibii@users.noreply.github.com>
2024-05-30 23:57:00 +0000

    Created Frame that holds all widgets

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 from tkinter import*
-import quiz_window #This imports the quiz window program
 
 #Created a window and set a background colour
 window = Tk()
-window.geometry("300x300")
+window.geometry("1280x800")
 window.title("My App")
 window['bg']='#2C2A64'
+
+#Creating a frame that holds all the widgets
+start_frame = Frame(window, width=1280, height=800, background='#2C2A64')
+start_frame.pack()
+#Maintains frame size with specified width and height
+start_frame.pack_propagate(0)
 
 #Added my logo image and edited the appearance
 img = PhotoImage(file="applogo.png")
 #This resizes the original picture by a factor of 6
 img = img.subsample(6) 
 #This allows the gray border of the picture to disappear
-label = Label(window,image=img,borderwidth=0, highlightthickness=0)
+label = Label(start_frame,image=img,borderwidth=0, highlightthickness=0)
 #This allows the picture to be found on the left and north west of the page
 label.pack(side=LEFT, anchor=NW)
 
 #Text displaying app name
-text_label = Label(window, text="My Goalie", font=("Georgia", 48), fg="#98D8DD", bg="#2C2A64")
+text_label = Label(start_frame, text="My Goalie", font=("Georgia", 48), fg="#98D8DD", bg="#2C2A64")
 #Positioning of this text
 text_label.place(relx=0.09, rely=0.01, anchor=NW)
 
 #Text displaying the yellow text
-text_label2 = Label(window, text="Are you curious to discover and work towards a personalised goal?", font =("Verdana", 25),
+text_label2 = Label(start_frame, text="Are you curious to discover and work towards a personalised goal?", font =("Verdana", 25),
 fg="#FBFF37", bg="#2C2A64")
 #Positioning of this text
 text_label2.place(relx=0.5, rely=0.3, anchor=CENTER)
 
 #Text displaying the green text
-text_label3 = Label(window, text="Take the quiz below to begin your journey towards success!" , font =("Verdana", 20),
+text_label3 = Label(start_frame, text="Take the quiz below to begin your journey towards success!" , font =("Verdana", 20),
 fg="#A6DF05", bg="#2C2A64")
 #Positioning of this text 
 text_label3.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 #Created a button to take quiz + designed the appearance
-quiz_button = Button(window,text= "Take the Quiz",command=open_quiz_window, font=("Arial", 37), bg='#A20202', fg='white', borderwidth=7,)
+quiz_button = Button(start_frame,text= "Take the Quiz", font=("Arial", 37), bg='#A20202', fg='white', borderwidth=7,)
 quiz_button.place(relx=0.5, rely=0.8, anchor='s')
 
 
