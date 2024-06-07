4e8d0a97b6ef69ecfb9ed33a2159e0dc7bfd5372
annasibii <160798608+annasibii@users.noreply.github.com>
2024-06-07 01:02:18 +0000

    Created IntVar and Radiobuttons on Quiz Frame

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from tkinter import*
 def go_to_quiz_frame():
   start_frame.pack_forget()
   quiz_frame.pack()
-  # Hide the start frame
-  #start_frame.pack_forget()
-  #quiz_frame.pack()
+  #Hide the start frame 
 
 
 #Created a window and set a background colour
@@ -58,11 +56,28 @@ quiz_button.place(relx=0.5, rely=0.75, anchor=S)
 
 ######## Quiz Frame ########
 
-quiz_frame = Frame(window, width=1280, height=800, background='red')
+quiz_frame = Frame(window, width=1280, height=800, background='#2C2A64')
 
+#Text displaying the yellow text
+text_label2 = Label(quiz_frame, text="What area would you like your goal to focus on?", font =("Verdana", 30),
+fg="#FBFF37", bg="#2C2A64")
+#Positioning of this text
+text_label2.place(relx=0.5, rely=0.15, anchor=CENTER)
 
-#Added my logo image and edited the appearance
+# Define an IntVar variable to store the selected radio button
+selected_var = IntVar()
 
+# Creating radio buttons - ensures that only one box can be checked
+productivity_radio = Radiobutton(quiz_frame, text="Productivity", font=("Verdana", 25),fg="#A6DF05", bg="#2C2A64", variable=selected_var, value=1, padx=20, pady=10,borderwidth=0, highlightthickness=0)
+
+health_radio = Radiobutton(quiz_frame, text="Health", font=("Verdana", 25),fg="#A6DF05", bg="#2C2A64", variable=selected_var, value=2, padx=20, pady=10,borderwidth=0, highlightthickness=0)
+
+# Positioning the radio buttons
+productivity_radio.place(relx=0.2, rely=0.4, anchor=W)
+health_radio.place(relx=0.2, rely=0.6, anchor=W)
+
+quiz_button = Button(quiz_frame,text= "Next", font=("Arial", 30), bg='#A20202', fg='white', borderwidth=7,)
+quiz_button.place(relx=0.8, rely=0.75, anchor=S)
 
 
 
