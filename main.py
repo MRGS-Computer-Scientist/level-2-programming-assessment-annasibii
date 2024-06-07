115e3b76c9db8ce5aac2ec0e20023c94d111a386
annasibii <160798608+annasibii@users.noreply.github.com>
2024-06-07 00:20:08 +0000

    Created a new frame that works

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import*
 
 def go_to_quiz_frame():
-  pass
+  start_frame.pack_forget()
+  quiz_frame.pack()
   # Hide the start frame
   #start_frame.pack_forget()
   #quiz_frame.pack()
@@ -13,7 +14,7 @@ window.geometry("1280x800")
 window.title("My App")
 window['bg']='#2C2A64'
 
-top_frame = Frame(window, width=1280, height=100, bg='#2C2A64')
+top_frame = Frame(window, width=1280, height=85, bg='#2C2A64')
 #Keeping the same width and height of the frame. By default, frames only take up as much space as they need.
 top_frame.pack_propagate(False)
 top_frame.pack()
@@ -42,22 +43,22 @@ text_label.place(relx=0.09, rely=0.01, anchor=NW)
 text_label2 = Label(start_frame, text="Are you curious to discover and work towards a personalised goal?", font =("Verdana", 25),
 fg="#FBFF37", bg="#2C2A64")
 #Positioning of this text
-text_label2.place(relx=0.5, rely=0.3, anchor=CENTER)
+text_label2.place(relx=0.5, rely=0.25, anchor=CENTER)
 
 #Text displaying the green text
 text_label3 = Label(start_frame, text="Take the quiz below to begin your journey towards success!" , font =("Verdana", 20),
 fg="#A6DF05", bg="#2C2A64")
 #Positioning of this text 
-text_label3.place(relx=0.5, rely=0.5, anchor=CENTER)
+text_label3.place(relx=0.5, rely=0.45, anchor=CENTER)
 
 #Created a button to take quiz + designed the appearance
-quiz_button = Button(start_frame,text= "Take the Quiz", font=("Arial", 37), bg='#A20202', fg='white', borderwidth=7,)
-quiz_button.place(relx=0.5, rely=0.8, anchor=S)
+quiz_button = Button(start_frame,text= "Take the Quiz", font=("Arial", 37), bg='#A20202', fg='white', borderwidth=7,command=go_to_quiz_frame)
+quiz_button.place(relx=0.5, rely=0.75, anchor=S)
 
 
 ######## Quiz Frame ########
 
-#quiz_frame = Frame(window, width=1280, height=800, background='#2C2A64')
+quiz_frame = Frame(window, width=1280, height=800, background='red')
 
 
 #Added my logo image and edited the appearance
